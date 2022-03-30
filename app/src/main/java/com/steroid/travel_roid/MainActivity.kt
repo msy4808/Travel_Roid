f2e9e0bf17b07c0e0e09d7753b2e3168455e9812
문 성 운 <mkik90911@naver.com>
2022-03-30 15:36:48 +0900

    test

--- app/src/main/java/com/steroid/travel_roid/MainActivity.kt
+++ app/src/main/java/com/steroid/travel_roid/MainActivity.kt
@@ -2,10 +2,12 @@ package com.steroid.travel_roid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this, "프로젝트 시작",Toast.LENGTH_SHORT).show()
     }
 }
