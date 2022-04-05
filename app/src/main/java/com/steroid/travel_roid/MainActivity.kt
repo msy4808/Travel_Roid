8075ecf873cb85c6e6e46165ffb93d9ad91d09d2
문 성 운 <mkik90911@naver.com>
2022-04-05 22:26:50 +0900

    TEST UI 1

--- app/src/main/java/com/steroid/travel_roid/MainActivity.kt
+++ app/src/main/java/com/steroid/travel_roid/MainActivity.kt
@@ -2,6 +2,8 @@ package com.steroid.travel_roid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
