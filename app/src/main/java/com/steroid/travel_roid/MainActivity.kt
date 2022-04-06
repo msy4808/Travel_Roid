63ede386a6991854946a4a9f9c0e546659c61198
문 성 운 <mkik90911@naver.com>
2022-04-06 16:21:14 +0900

    번역 API(파파고) 테스트 성공

--- app/src/main/java/com/steroid/travel_roid/MainActivity.kt
+++ app/src/main/java/com/steroid/travel_roid/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
