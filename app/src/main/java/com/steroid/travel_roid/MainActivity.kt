3e77647dbb6a01a8cf6bbaacb4e121b574e543cf
문 성 운 <mkik90911@naver.com>
2022-04-10 15:31:34 +0900

    파파고 언어감지 API적용

--- app/src/main/java/com/steroid/travel_roid/MainActivity.kt
+++ app/src/main/java/com/steroid/travel_roid/MainActivity.kt
@@ -2,9 +2,14 @@ package com.steroid.travel_roid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.core.widget.doOnTextChanged
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,11 +18,31 @@ class MainActivity : AppCompatActivity() {
         val userEnterText: EditText = findViewById(R.id.textIn)
         val btn: Button = findViewById<Button>(R.id.btn)
         val result: TextView = findViewById(R.id.result)
+        val textWatcher = object: TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                val timer = Timer()
+                timer.schedule(object: TimerTask(){
+                    override fun run() {
+                        val detectLangs = DetectLangs(p0.toString())
+                        Log.d("TextWatcher 테스트", p0.toString())
+                        detectLangs.getlangCode()
+                    }
+                },3000)
+            }
+        }
 
         btn.setOnClickListener {
-            val startApp = TranslateTask(userEnterText.text.toString())
-            result.text = startApp.execute().get()
+            val translate = TranslateTask(userEnterText.text.toString())
+            result.text = translate.execute().get()
         }
+
+        userEnterText.addTextChangedListener(textWatcher)
 
     }
 }
