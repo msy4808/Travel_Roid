4f2776206017485d04a54868d69f57219cb38b05
jung0302 <seungjung32@gmail.com>
2022-05-11 14:31:03 +0900

    GPS 테스트

--- app/src/main/java/com/steroid/travel_roid/LoginActivity.kt
+++ app/src/main/java/com/steroid/travel_roid/LoginActivity.kt
@@ -1,36 +1,26 @@
 package com.steroid.travel_roid
 
-import android.app.Application
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.Window
-import android.view.WindowManager
 import android.widget.ImageButton
-import android.widget.TextView
 import android.widget.Toast
+
 import com.kakao.sdk.auth.model.OAuthToken
-import com.kakao.sdk.common.KakaoSdk
-import com.kakao.sdk.common.model.AuthErrorCause
 import com.kakao.sdk.common.util.Utility
-import com.kakao.sdk.user.UserApiClient
 import com.kakao.sdk.common.model.AuthErrorCause.*
-
-
-
+import com.kakao.sdk.user.UserApiClient
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_login2)
 
         // 로그인 정보 확인
         UserApiClient.instance.accessTokenInfo { tokenInfo, error ->
             if (error != null) {
                 Toast.makeText(this, "토큰 정보 보기 실패", Toast.LENGTH_SHORT).show()
-
             }
             else if (tokenInfo != null) {
                 Toast.makeText(this, "토큰 정보 보기 성공", Toast.LENGTH_SHORT).show()
@@ -48,28 +38,28 @@ class LoginActivity : AppCompatActivity() {
         val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
             if (error != null) {
                 when {
-                    error.toString() == AuthErrorCause.AccessDenied.toString() -> {
+                    error.toString() == AccessDenied.toString() -> {
                         Toast.makeText(this, "접근이 거부 됨(동의 취소)", Toast.LENGTH_SHORT).show()
                     }
-                    error.toString() == AuthErrorCause.InvalidClient.toString() -> {
+                    error.toString() == InvalidClient.toString() -> {
                         Toast.makeText(this, "유효하지 않은 앱", Toast.LENGTH_SHORT).show()
                     }
-                    error.toString() == AuthErrorCause.InvalidGrant.toString() -> {
+                    error.toString() == InvalidGrant.toString() -> {
                         Toast.makeText(this, "인증 수단이 유효하지 않아 인증할 수 없는 상태", Toast.LENGTH_SHORT).show()
                     }
-                    error.toString() == AuthErrorCause.InvalidRequest.toString() -> {
+                    error.toString() == InvalidRequest.toString() -> {
                         Toast.makeText(this, "요청 파라미터 오류", Toast.LENGTH_SHORT).show()
                     }
-                    error.toString() == AuthErrorCause.InvalidScope.toString() -> {
+                    error.toString() == InvalidScope.toString() -> {
                         Toast.makeText(this, "유효하지 않은 scope ID", Toast.LENGTH_SHORT).show()
                     }
-                    error.toString() == AuthErrorCause.Misconfigured.toString() -> {
+                    error.toString() == Misconfigured.toString() -> {
                         Toast.makeText(this, "설정이 올바르지 않음(android key hash)", Toast.LENGTH_SHORT).show()
                     }
-                    error.toString() == AuthErrorCause.ServerError.toString() -> {
+                    error.toString() == ServerError.toString() -> {
                         Toast.makeText(this, "서버 내부 에러", Toast.LENGTH_SHORT).show()
                     }
-                    error.toString() == AuthErrorCause.Unauthorized.toString() -> {
+                    error.toString() == Unauthorized.toString() -> {
                         Toast.makeText(this, "앱이 요청 권한이 없음", Toast.LENGTH_SHORT).show()
                     }
                     else -> { // Unknown
@@ -79,10 +69,10 @@ class LoginActivity : AppCompatActivity() {
             }
             else if (token != null) {
                 Toast.makeText(this, "로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, SecondActivity::class.java)
+                startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+                finish()
             }
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
-            finish()
         }
 
 
@@ -91,24 +81,15 @@ class LoginActivity : AppCompatActivity() {
         kakao_login_button.setOnClickListener {
             if(UserApiClient.instance.isKakaoTalkLoginAvailable(this)){
                 UserApiClient.instance.loginWithKakaoTalk(this, callback = callback)
+
+
             }else{
                 UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
             }
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
+
 }
+
 
 
